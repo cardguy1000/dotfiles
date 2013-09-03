d2694e067af50d48082520c959dcafa3f4cda592
Jeff Kackley <kackleyjm@gmail.com>
2013-09-02 23:50:07 -0500

    Added sourcing bashrc to bash_profile

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vim"    # list of files/folders to symlink in homedir
+files="bashrc vim bash_profile"    # list of files/folders to symlink in homedir
 
 ##########
 
